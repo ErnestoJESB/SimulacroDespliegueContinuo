d266ea4cf7503f011ca406bc6cf8151070294f1a
ErnestoJESB <21393154@utcancun.edu.mx>
2024-11-04 19:14:21 -0500

    Error en require

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 // hello.test.js
 test('imprime Hola, Mundo', () => {
   console.log = jest.fn(); // Mock console.log
-  require('./hello'); // Cargar el archivo hello.js
+  require('./hello_world'); // Cargar el archivo hello.js
   expect(console.log).toHaveBeenCalledWith("Hola, Mundo");
 });
